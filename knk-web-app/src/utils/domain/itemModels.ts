bf1a16489566e2e3ebd64e7ffd796d9a5a71e43a
Pandi Oldenzeel (Mac) <pandi.oldenzeel@hotmail.nl>
2025-01-12 14:46:02 +0100

    Added classes for Communication layer with API
    Also added rxjs package.

---
+++ knk-web-app/src/utils/domain/itemModels.ts
@@ -0,0 +1,10 @@
+export class ItemDTO {
+    Id: number = -1;
+    Name: string;
+    DisplayName: string;
+    BasePrice: number;
+    BaseItemId: number;
+    CategoryId: number;
+    GradeId: number;
+    ItemtypeId: number;
+}
